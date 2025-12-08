e4b76ec83494a2e03fd2306d52e7dc7e8eb6db1c
tehreemrehman-hash <tehreemrehman@gmail.com>
2025-12-08 05:32:25 +0000

    Remove password protection

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,50 +72,6 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-# --- SIMPLE AUTHENTICATION ---
-# To make the app private, we check for a password.
-# Default password is 'carepathiq' if not set in secrets.
-def check_password():
-    """Returns `True` if the user has the correct password."""
-    
-    # 1. Get password from secrets or use default
-    # In Streamlit Cloud, set 'APP_PASSWORD' in the Secrets management.
-    correct_password = st.secrets.get("APP_PASSWORD", "carepathiq")
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == correct_password:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "🔒 This application is private. Please enter the password:", 
-            type="password", 
-            on_change=password_entered, 
-            key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "🔒 This application is private. Please enter the password:", 
-            type="password", 
-            on_change=password_entered, 
-            key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return True
-
-if not check_password():
-    st.stop()
-
 # --- CUSTOM CSS: GLOBAL DARK BROWN THEME ---
 st.markdown("""
 <style>
